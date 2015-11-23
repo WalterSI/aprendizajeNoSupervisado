a9df1b96c07a411027c3bd01e871aa39274cc77c
WalterSI <WalterSI@users.noreply.github.com>
2015-11-22 19:48:38 -0500

    Update createPatches.py

--- createPatches.py
+++ createPatches.py
@@ -8,14 +8,16 @@ Created on Fri Nov 20 22:07:24 2015
 # Based on CS294A/CS294W Programming Assignment Starter Code
 from numpy  import *
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import scipy.io
 import h5py
+import datetime
 
 #def getPatches(numPatches, patchSize):
 
+dateInit = datetime.datetime.now()
 patchesPerImage = 2
-numImages = 1000
+numImages = 100000
 numPatches = numImages*patchesPerImage
 patchSize = 8
 imageSize = 96
@@ -23,52 +25,28 @@ imageSize = 96
 
 f = h5py.File('unlabeled.mat')
 X=f["X"]
-#print len(X[:,0])
-#print X[:,0]
-
-#images = np.zeros((imageSize, imageSize, 3))
-imagesR = np.zeros((imageSize, imageSize, numImages))
-imagesG = np.zeros((imageSize, imageSize, numImages))
-imagesB = np.zeros((imageSize, imageSize, numImages))
-
-for i in range(numImages):
-    valor1 = imageSize*imageSize
-    valor2 = valor1 + imageSize*imageSize
-    valor3 = valor2 + imageSize*imageSize
-    imagesR[:, :, i] = X[0:valor1,i].reshape(imageSize, imageSize)
-    imagesG[:, :, i] = X[valor1:valor2,i].reshape(imageSize, imageSize)
-    imagesB[:, :, i] = X[valor2:valor3,i].reshape(imageSize, imageSize)
-
-#images[:, :, 0] = imagesR[:, :, 5]
-#images[:, :, 1] = imagesG[:, :, 5]
-#images[:, :, 2] = imagesB[:, :, 5]
-#imgplot1 = plt.imshow(images)    
 
 patchesR = zeros((numPatches, patchSize*patchSize))
 patchesG = zeros((numPatches, patchSize*patchSize))
 patchesB = zeros((numPatches, patchSize*patchSize))
 	
 for i in range(numImages):
-    imgR = imagesR[:,:,i]
-    imgG = imagesG[:,:,i]
-    imgB = imagesB[:,:,i]
+    
+    valor1 = imageSize*imageSize
+    valor2 = valor1 + imageSize*imageSize
+    valor3 = valor2 + imageSize*imageSize
+    imgR = X[0:valor1,i].reshape(imageSize, imageSize)
+    imgG = X[valor1:valor2,i].reshape(imageSize, imageSize)
+    imgB = X[valor2:valor3,i].reshape(imageSize, imageSize)
     
     for j in range(patchesPerImage):
 
         x = random.randint(imgR.shape[0] - patchSize)
         y = random.randint(imgR.shape[1] - patchSize)
 
-        patchR = np.zeros((patchSize, patchSize, 3))
-        patchG = np.zeros((patchSize, patchSize, 3))
-        patchB = np.zeros((patchSize, patchSize, 3))
-    
-        patchR = imgR[x:x+patchSize, y:y+patchSize]
-        patchG = imgG[x:x+patchSize, y:y+patchSize]
-        patchB = imgB[x:x+patchSize, y:y+patchSize]
-    
-        patchesR[i+j, :] = patchR.reshape(1, patchSize*patchSize)
-        patchesG[i+j, :] = patchG.reshape(1, patchSize*patchSize)
-        patchesB[i+j, :] = patchB.reshape(1, patchSize*patchSize)
+        patchesR[i+j, :] = imgR[x:x+patchSize, y:y+patchSize].reshape(1, patchSize*patchSize)
+        patchesG[i+j, :] = imgG[x:x+patchSize, y:y+patchSize].reshape(1, patchSize*patchSize)
+        patchesB[i+j, :] = imgB[x:x+patchSize, y:y+patchSize].reshape(1, patchSize*patchSize)
 
 print "paso for"
 # Remove DC (mean of images)
@@ -97,6 +75,7 @@ for i in range(numPatches):
     imagesPatches[128:192, i] = patchesB[i,:]
 
 scipy.io.savemat('walterPatches.mat',{'patches':imagesPatches})
-
+duration = datetime.datetime.now() - dateInit
+print duration
 #print imagesPatches[:,0]
 #imgplot = plt.imshow(imagesPatches)
